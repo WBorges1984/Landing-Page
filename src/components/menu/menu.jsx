33ed2d91cb567bc2ffeba3264ca09d8b3c070c62
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2024-07-28 18:25:40 -0300

    Menu-banner mobile

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './menu.css'
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
+import { RiMenu2Fill } from "react-icons/ri";
 
 function Menu(props) {
   return (
     <div className={props.white ? "containerMenu fontWhite" : "containerMenu" }>
+      <div className="menuHamburguer">
+      <RiMenu2Fill />
+      </div>
       <div className="logoMenu">
         <h4>borges</h4>
       </div>
@@ -19,10 +23,12 @@ function Menu(props) {
             <FaInstagram />
             <FaFacebookF />
           </div>
-
-        
-        
       </div>
+          <div className="linkMenuHamburguer">
+            <a href="#beneficios">o que fazemos</a>
+            <a href="#ajudamos">como te ajudamos</a>
+            <a href="#falaconosco">fale conosco</a>
+          </div>
     </div>
   )
 }
