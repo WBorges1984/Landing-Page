d41ba4c7dd2c6bcdebd6ce0c0e069708f1b0962d
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2024-07-28 23:50:48 -0300

    ajuste

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -6,21 +6,53 @@ import { RiMenu2Fill } from "react-icons/ri";
 
 function Menu(props) {
   const [open, setOpen] = useState(false);
+  const [showBurguer, setShowBurguer] = useState(false);
 
-  useEffect(()=>{
-    if(open){
-      
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // Adiciona o event listener para o resize
+    window.addEventListener('resize', handleResize);
+    
+    if(windowSize.width < 768){
+      setShowBurguer(true)
+      console.log(windowSize)
+     
+    }else{
+     
+      setShowBurguer(false)
     }
-  },[open])
+    // Remove o event listener na desmontagem do componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, [windowSize]);
+
 
   function openBurguer(){
     setOpen(!open)
   }
+
+
   return (<>
     <div className={props.white ? "containerMenu fontWhite" : "containerMenu" }>
-      <div className= {open ? "menuHamburguer rotate" : "menuHamburguer" } onClick={openBurguer}>
-      <RiMenu2Fill/>
-      </div>
+      
+      {showBurguer && (
+        <div className= "menuHamburguer " onClick={openBurguer} style={showBurguer ? {display: 'flex'}: {display: 'none'}}>
+        <RiMenu2Fill/>
+        </div>
+      )}
 
       <div className="logoMenu">
         <h4>borges</h4>
